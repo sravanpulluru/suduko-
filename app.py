4e967561fae6753ae1e0339466a3310b22a3bd2e
venkata sravan pulluru <venkatasravan_pulluru@srmap.edu.in>
2026-04-13 11:42:10 +0530

    Update app to use environment variable for port

--- app.py
+++ app.py
@@ -59,3 +59,7 @@ def api_validate():
 if __name__ == "__main__":
     app.run(host="127.0.0.1", port=5000, debug=True)
 
+if __name__ == "__main__":
+    import os
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
